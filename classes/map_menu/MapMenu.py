8b52f2622184bcbcebe71a7db385d0b3d5e851c9
Antônio Marco da Silva <antonio.silva@bludata.com.br>
2018-07-04 17:00:32 -0300

    Stating map menu

--- classes/map_menu/MapMenu.py
+++ classes/map_menu/MapMenu.py
@@ -5,41 +5,77 @@ class DataMenu(TabMenuInterface):
 
     name = 'Map'
     surface = None
+    places = []
+
+    saveVersion = 1
+
+    #User-view's Zoom/Position
+    viewZoom = 1.0
+    viewPosX = 0.0
+    viewPosY = 0.0
+
+    cursorRadius = 32
+    cursorPosX = 0.0
+    cursorPosY = 0.0
+
+    mapType = 0
+    mapImage = 0
+    mapSize = 640
+    mapScale = 2
+
+    cursorSize = 16
+    markerSizeSm = 32
+    markerSizeBg = 48
+    halfMarkerSizeSm = markerSizeSm/2
+    halfMarkerSizeBg = markerSizeBg/2
+
+    #used to implement mouse-acceleration:
+    moveStartTick 0
+    lastMoveTick = 0
+
+    mapImageSize = (mapSize * mapScale)
+
 
     def __init__(self):
-        print('Initialize tab Data', end='')
+        print('Initialize tab Menu')
         self.size = (int(os.getenv('SCREEN_WIDTH')), int(int(os.getenv('SCREEN_HEIGHT'))*0.9))
         self.surface = pygame.Surface(self.size)
-        self.sub_menu_position = (0, int(self.size[1]*0.1))
-        self.menus = []
-        self.selected_menu = None
-        self.selected_menu_index = 0
+        print('Generating cursor', end='')
+        self.generate_cursor()
+        print('(done)')
         print('(done)')
 
-    def prev_sub_menu(self):
-        if len(self.menus) == 0:
-            raise IndexError
-        if self.selected_menu_index-1 >= 0:
-            self.selected_menu_index -= 1
-            self.selected_menu = self.menus[self.selected_menu_index]
-        else:
-            self.selected_menu_index = len(self.selected_menu)-1
-            self.selected_menu = self.menus[self.selected_menu_index]
 
-    def next_sub_menu(self):
-        if len(self.menus) == 0:
-            raise IndexError
-        if len(self.menus) <= self.selected_menu_index+1:
-            self.selected_menu_index += 1
-            self.selected_menu = self.menus[self.selected_menu_index]
-        else:
-            self.selected_menu_index = 0
-            self.selected_menu = self.menus[self.selected_menu_index]
-
-    def add_sub_menu(self, menu):
-        self.menus.append(menu)
-        if self.selected_menu == None:
-            self.selected_menu = self.menus[0]
+    def generate_cursor(self):
+       # Generate cursor-box image:
+        cursBoxSize = (2 * self.cursorRadius)
+        thirdSize = (cursBoxSize / 3)
+        self.cursorBox = pygame.Surface((cursBoxSize, cursBoxSize))
+        self.cursorBox.fill((0,0,0))
+        lnSize = 2
+        pygame.draw.rect(self.cursorBox, (255,255,255), (self.cursorRadius-(lnSize/2),self.cursorRadius-(lnSize/2),lnSize,lnSize), 0)
+        pygame.draw.lines(self.cursorBox, (255,255,255), False, [
+            (lnSize,thirdSize),
+            (lnSize,lnSize),
+            (thirdSize,lnSize)
+        ], lnSize)
+        pygame.draw.lines(self.cursorBox, (255,255,255), False, [
+            (cursBoxSize-thirdSize,lnSize),
+            (cursBoxSize-lnSize,lnSize),
+            (cursBoxSize-lnSize,thirdSize)
+        ], lnSize)
+        pygame.draw.lines(self.cursorBox, (255,255,255), False, [
+            (lnSize,cursBoxSize-thirdSize),
+            (lnSize,cursBoxSize-lnSize),
+            (thirdSize,cursBoxSize-lnSize)
+        ], lnSize)
+        pygame.draw.lines(self.cursorBox, (255,255,255), False, [
+            (cursBoxSize-thirdSize, cursBoxSize-lnSize),
+            (cursBoxSize-lnSize, cursBoxSize-lnSize),
+            (cursBoxSize-lnSize, cursBoxSize-thirdSize)
+        ], lnSize)
+        self.cursorBox = self.cursorBox.convert()
+        self.resetCursorPos()
 
     def event(self, event):
         pass
@@ -49,9 +85,4 @@ class DataMenu(TabMenuInterface):
 
     def draw(self):
         self.surface.fill((255,255,255))
-        for menu in (self.menus):
-            pass
-        if self.selected_menu:
-            self.selected_menu.draw()
-            self.surface.blit(self.selected_menu.surface, self.sub_menu_position)
         pass
